c51daaa77e7caca61220f6c3c92598ea6eb643d6
sunzh <zhengxi.mo@jniu.com>
2022-11-23 10:11:19 +0800

    feat: many feature

--- src/service/lucky.ts
+++ src/service/lucky.ts
@@ -1,6 +1,6 @@
 import axios from '../request/axios'
 
-/** 查询一个抽奖接口 POST /admin/assessment2/assessment2/interaction-games/getByC */
+/** 查询抽奖信息接口 POST /admin/assessment2/assessment2/interaction-games/getByC */
 export async function luckyWheelGet(data: any) {
   return axios
     .post(`/assessment/interaction-games/getByC`, data)
@@ -11,10 +11,21 @@ export async function luckyWheelGet(data: any) {
     });
 }
 
-/** 查询一个抽奖接口 POST /admin/assessment2/assessment2/interaction-games/play */
+/** 开始抽奖接口 POST /admin/assessment2/assessment2/interaction-games/play */
 export async function luckyWheelPlay(data: any) {
   return axios
     .post(`/assessment/interaction-games/play`, data)
+    .then(response=> {
+      if (response.data) {
+        return response.data;
+      }
+    });
+}
+
+/** 中奖列表接口 POST /admin/assessment2/assessment2/interaction-prize-record/list */
+export async function luckyWheelPrizeList(data: any) {
+  return axios
+    .post(`/assessment/interaction-prize-record/list`, data)
     .then(response=> {
       if (response.data) {
         return response.data;
